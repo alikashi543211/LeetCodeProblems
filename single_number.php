c1344417ee91612cd5ba10a552772d3d77fb9769
alikashi543211 <alikashi54321@gmail.com>
2023-08-15 10:58:01 +0500

    single number with xor operator

---
+++ single_number.php
@@ -0,0 +1,21 @@
+<?php
+    echo "<h1 style='margin-top:40px;text-align:center;'> Single Number </h1>";
+
+    function singleNumber($array)
+    {
+        $a = 0;
+        foreach($array as $val)
+        {
+            $a = $a ^ $val;
+        }
+
+        return $a;
+    }   
+
+    // Example 1 : [2, 3, 2, 4, 4]
+    // Example 2 : [4, 5, 9, 5, 4, 2, 10, 2, 10]
+    
+    $result = singleNumber($array = [4, 5, 9, 5, 4, 2, 10, 2, 10]);
+
+    echo $result;
+?>
